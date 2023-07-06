5a4b7f46a4be36674a578a36caa6ed405e796a9d
CHAN W1LL <303661031@qq.com>
2023-07-06 13:54:05 +0800

    hello,react

--- hello_react/src/App.js
+++ hello_react/src/App.js
@@ -1,10 +1,13 @@
 import React,{Component} from "react";
+import Hello from "./components/Hello/Hello";
+import Wlecome from "./components/Welcome/Welcome";
 
 class App extends Component{
   render(){
     return (
         <div>
-          hello,react!
+          <Hello/>
+          <Wlecome/>
         </div>
     )
   }
---
+++ hello_react/src/components/Welcome/Welcome.js
@@ -0,0 +1,8 @@
+import React,{Component} from "react";
+import './Welcome.css'
+
+export default class Wlecome extends Component{
+    render() {
+        return <h2 className="demo">Welcome</h2>
+    }
+}
